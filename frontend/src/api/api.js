54443fe457a100c1fcd3fa0bcd1cf50bd25b9e3f
setoudie <setoudie@gmail.com>
2025-01-15 01:49:14 +0000

    Add PyTorch CPU-specific dependencies to requirements.txt
    Included the PyTorch CPU wheel index URL and updated dependencies for torch, torchaudio, and torchvision to their CPU-specific versions. This ensures compatibility with systems lacking GPU support.

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:5000/transcribe";
+const API_URL = "https://asr-application.onrender.com/transcribe";
 
 export const sendAudioFile = async (audioFile) => {
   try {
